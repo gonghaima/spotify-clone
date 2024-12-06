1bf1fb7908bd7d9ef894449fad7cf48d6304d706
Steven Gong <gonghaima@hotmail.com>
2024-12-07 00:44:05 +1100

    add user route

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import userRoutes from './routes/user.route.js';
+
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use('/api/users', userRoutes);
 
 app.listen(PORT, () => {
   console.log('Server is running on port ' + PORT);
