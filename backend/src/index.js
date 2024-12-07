32aff8ffd327895b2bdb84f89c475af9c2fe4558
Steven Gong <gonghaima@hotmail.com>
2024-12-08 03:03:22 +1100

    add the rest of the routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+import adminRoutes from './routes/admin.route.js';
+import songRoutes from './routes/song.route.js';
+import albumRoutes from './routes/album.route.js';
+import statsRoutes from './routes/stat.route.js';
 
 dotenv.config();
 
@@ -11,6 +16,11 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/admin', adminRoutes);
+app.use('/api/songs', songRoutes);
+app.use('/api/albums', albumRoutes);
+app.use('/api/stats', statsRoutes);
 
 app.listen(PORT, () => {
   console.log('Server is running on port ' + PORT);
