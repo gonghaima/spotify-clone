d1951642e711ae2f5db38b91b01861934e3bfe87
Steven Gong <gonghaima@hotmail.com>
2024-12-15 09:09:00 +1100

    deleteSong

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { getAdmin, createSong } from '../controller/admin.controller.js';
+import { createSong, deleteSong } from '../controller/admin.controller.js';
 
 const router = Router();
 
 router.post('/songs', protectRoute, requireAdmin, createSong);
+router.delete("/songs/:id", deleteSong);
 
 export default router;
