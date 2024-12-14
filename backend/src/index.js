504009f63f219de237c5a99f8693d2242e789c61
Steven Gong <gonghaima@hotmail.com>
2024-12-14 11:27:22 +1100

    create song & middleware catch error

--- backend/src/index.js
+++ backend/src/index.js
@@ -38,6 +38,16 @@ app.use('/api/songs', songRoutes);
 app.use('/api/albums', albumRoutes);
 app.use('/api/stats', statsRoutes);
 
+// error handler
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message:
+      process.env.NODE_ENV === 'production'
+        ? 'Internal server error'
+        : err.message,
+  });
+});
+
 app.listen(PORT, () => {
   console.log('Server is running on port ' + PORT);
   connectDB();
