11dad17566e272975ac69746fc8af82d764f78dc
Steven Gong <gonghaima@hotmail.com>
2024-12-16 16:31:02 +1100

    createAlbum

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
-import { createSong, deleteSong } from '../controller/admin.controller.js';
+import {
+  createAlbum,
+  createSong,
+  deleteSong,
+} from '../controller/admin.controller.js';
 
 const router = Router();
 
 router.post('/songs', protectRoute, requireAdmin, createSong);
-router.delete("/songs/:id", deleteSong);
+router.delete('/songs/:id', deleteSong);
+
+router.post('/albums', createAlbum);
 
 export default router;
