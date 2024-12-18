ac4fb7a6675ee3974a31cc88d5c005142f14100d
Steven Gong <gonghaima@hotmail.com>
2024-12-18 19:08:16 +1100

    checkAdmin

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -2,11 +2,14 @@ import { Router } from 'express';
 import {
   createAlbum,
   createSong,
+  checkAdmin,
   deleteSong,
   deleteAlbum,
 } from '../controller/admin.controller.js';
 
 const router = Router();
+
+router.get("/check",protectRoute, requireAdmin, checkAdmin);
 
 router.post('/songs', protectRoute, requireAdmin, createSong);
 router.delete('/songs/:id', deleteSong);
