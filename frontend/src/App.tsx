ea23e7dc3ae67de4cdd464331e98039aea1167e1
Steven Gong <gonghaima@hotmail.com>
2025-01-04 02:57:35 +1300

    AuthenticateWithRedirectCallback from /sso-callback to /auth-callback

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,24 @@
 import { Route, Routes } from 'react-router-dom';
 
-import HomePage from "./pages/home/HomePage";
-import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage.tsx";
+import HomePage from './pages/home/HomePage';
+import AuthCallbackPage from './pages/auth-callback/AuthCallbackPage.tsx';
+import { AuthenticateWithRedirectCallback } from '@clerk/clerk-react';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route
+          path="/sso-callback"
+          element={
+            <AuthenticateWithRedirectCallback
+              signUpForceRedirectUrl={'/auth-callback'}
+            />
+          }
+        />
         <Route path="/auth-callback" element={<AuthCallbackPage />} />
-      </Routes>
+      </Routes> 
     </>
   );
 }
--- frontend/src/pages/auth-callback/AuthCallbackPage.tsx
+++ frontend/src/pages/auth-callback/AuthCallbackPage.tsx
@@ -3,5 +3,9 @@ import React from 'react';
 type Props = {};
 
 export default function AuthCallbackPage({}: Props) {
-  return <div>AuthCallbackPage</div>;
+  return (
+    <div className="h-screen w-full bg-black flex items-center justify-center">
+      Card
+    </div>
+  );
 }
