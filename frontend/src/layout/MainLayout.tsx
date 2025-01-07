af9821ad0fb18c6e950119e2ca55c9a17bd501e7
Steven Gong <gonghaima@hotmail.com>
2025-01-08 03:47:58 +0800

    initialize left sidebar

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -1,6 +1,6 @@
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
 import { Outlet } from "react-router-dom";
-// import LeftSidebar from "./components/LeftSidebar";
+import LeftSidebar from "./components/LeftSidebar.tsx";
 // import FriendsActivity from "./components/FriendsActivity"; 
 // import AudioPlayer from "./components/AudioPlayer";
 // import { PlaybackControls } from "./components/PlaybackControls";
@@ -25,7 +25,7 @@ const MainLayout = () => {
 				{/* <AudioPlayer /> */}
 				{/* left sidebar */}
 				<ResizablePanel defaultSize={20} minSize={isMobile ? 0 : 10} maxSize={30}>
-					LeftSidebar
+					<LeftSidebar />
 				</ResizablePanel>
 
 				<ResizableHandle className='w-2 bg-black rounded-lg transition-colors' />
