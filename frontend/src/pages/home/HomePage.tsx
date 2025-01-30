101f31292f5f3a308c106ec93275398efe17875a
Steven Gong <gonghaima@hotmail.com>
2025-01-30 11:38:50 +1100

    call / test song api

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,8 +1,32 @@
 // Basic React component structure
 import Topbar from '@/components/Topbar';
-import React from 'react';
+import { useMusicStore } from '@/stores/useMusicStore';
+import React, { useEffect } from 'react';
 
 const HomePage: React.FC = () => {
+  const {
+    fetchFeaturedSongs,
+    fetchMadeForYouSongs,
+    fetchTrendingSongs,
+    isLoading,
+    madeForYouSongs,
+    featuredSongs,
+    trendingSongs,
+  } = useMusicStore();
+  console.log(
+    '🚀 ~ isLoading,madeForYouSongs,featuredSongs,trendingSongs,:',
+    isLoading,
+    madeForYouSongs,
+    featuredSongs,
+    trendingSongs
+  );
+
+  useEffect(() => {
+    fetchFeaturedSongs();
+    fetchMadeForYouSongs();
+    fetchTrendingSongs();
+  }, [fetchFeaturedSongs, fetchMadeForYouSongs, fetchTrendingSongs]);
+
   return (
     <div>
       <Topbar />
