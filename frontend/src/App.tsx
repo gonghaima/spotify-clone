4b8593e05259472b9b84effaf73b3abb7e48b453
Steven Gong <gonghaima@hotmail.com>
2025-02-15 07:11:02 +1100

    add initial admin page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import ChatPage from './pages/chat/ChatPage.tsx';
 import AlbumPage from './pages/album/AlbumPage.tsx';
 import { AuthenticateWithRedirectCallback } from '@clerk/clerk-react';
 import MainLayout from './layout/MainLayout.tsx';
+import AdminPage from "./pages/admin/AdminPage";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           }
         />
         <Route path="/auth-callback" element={<AuthCallbackPage />} />
+        <Route path="/admin" element={<AdminPage />} />
         <Route element={<MainLayout />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/cat" element={<ChatPage />} />
