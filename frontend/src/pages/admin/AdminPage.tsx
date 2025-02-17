7dac738bd6e2fef545d52cfe2402737b38642da9
Steven Gong <gonghaima@hotmail.com>
2025-02-17 21:14:34 +1100

    songs & albums tab

--- frontend/src/pages/admin/AdminPage.tsx
+++ frontend/src/pages/admin/AdminPage.tsx
@@ -3,8 +3,8 @@ import Header from './components/Header';
 import DashboardStats from './components/DashboardStats';
 import { Album, Music } from 'lucide-react';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import SongsTabContent from './components/SongsTabContent';
-import AlbumsTabContent from './components/AlbumsTabContent';
+// import SongsTabContent from './components/SongsTabContent';
+// import AlbumsTabContent from './components/AlbumsTabContent';
 import { useEffect } from 'react';
 import { useMusicStore } from '@/stores/useMusicStore';
 
@@ -27,6 +27,30 @@ function AdminPage() {
     >
       <Header />
       <DashboardStats />
+      <Tabs defaultValue="songs" className="space-y-6">
+        <TabsList className="p-1 bg-zinc-800/50">
+          <TabsTrigger
+            value="songs"
+            className="data-[state=active]:bg-zinc-700"
+          >
+            <Music className="mr-2 size-4" />
+            Songs
+          </TabsTrigger>
+          <TabsTrigger
+            value="albums"
+            className="data-[state=active]:bg-zinc-700"
+          >
+            <Album className="mr-2 size-4" />
+            Albums
+          </TabsTrigger>
+        </TabsList>
+        {/* <TabsContent value="songs">
+          <SongsTabContent />
+        </TabsContent>
+        <TabsContent value="albums">
+          <AlbumsTabContent />
+        </TabsContent> */}
+      </Tabs>
     </div>
   );
 }
